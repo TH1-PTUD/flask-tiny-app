653b7c3b4d2e2119cd4c8894c5d441187703038d
hoangailinh <hoangailinh11062004@gmail.com>
2025-02-24 15:49:59 +0700

    update file app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return "<h1> ẨM THỰC VIỆT NAM </h1>"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
